d38d0a72d95552a3dfcaaa11524c03854163d14c
khalilo1803 <47722569+khalilo1803@users.noreply.github.com>
2022-07-28 02:56:12 +0200

    pagination

--- sponsor/views.py
+++ sponsor/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Sponsor
 # Create your views here.
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def Display(request):
-    return render(request,'sponsor.html' ,{'sp' : Sponsor.objects.all()} )
+    sponsor_list = Sponsor.objects.all()
+    page = request.GET.get('page', 1)
+ 
+    paginator = Paginator(sponsor_list, 2)
+    try:
+        sponsors = paginator.page(page)
+    except PageNotAnInteger:
+        sponsors = paginator.page(1)
+    except EmptyPage:
+        sponsors = paginator.page(paginator.num_pages)
+    return render(request,'sponsor.html' ,{'sp' : sponsors} )
