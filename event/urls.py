c19b614067cb27e474f130dcb16a39c008c71c82
iheblafi <iheb.lafi@esprit.tn>
2022-08-26 15:15:17 +0100

    PDF

--- event/urls.py
+++ event/urls.py
@@ -2,5 +2,6 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
   path('', views.list_event,name='event'),
-  path('even/<int:id>', views.even, name = 'even')
+  path('even/<int:id>', views.even, name = 'even'),
+  path('addevent/', views.addev, name='addev'),
 ]
